1721b7462eddcb965040425ccde2c043f6d96598
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-06-28 16:40:24 +0000

    3-4 finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,23 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new                       
+      # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
   end
 
+  def create
+      # 
+    list = List.new(list_params)            # list=変数.new(引数) 
+      # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list.save
+      # 3. データをデータベースに保存するためのsaveメソッド実行
+    redirect_to '/top'
+      # 4. トップ画面へリダイレクト(遷移)
+      
+  end
+  
   def index
+    @lists = List.all
+      # インスタンス変数=listsテーブル全(変数へ代入)
   end
 
   def show
@@ -10,4 +25,14 @@ class ListsController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def list_params
+        # ストロングパラメータ
+    params.require(:list).permit(:title, :body)
+  end
+  
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'lists/new'
+  get 'lists/new'                    # 
   post 'lists' => 'lists#create'     # データ追加(保存)ルーティング
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+  get 'lists' => 'lists#index'       # indexアクション ルーティング
+  get 'lists/show'                   # 
+  get 'lists/edit'                   # 
+  get '/top' => 'homes#top'          # topページ？
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
