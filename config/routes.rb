735664b7555529eca2e07b7571fb4aadfc3b4a9a
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-01 06:19:24 +0000

    5-5 totyu02

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'lists/:id' => 'lists#show', as: 'list'
     # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-    # 
+    # ...
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
